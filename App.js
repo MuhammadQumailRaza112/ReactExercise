e3c026b7bee7f27abbdd6f623a6b3ca31e8abe6a
MuhammadQumailRaza112 <mqumailr@gmail.com>
2021-10-26 12:58:01 +0500

    React exercise done

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet,SafeAreaView, Text, View } from 'react-native';
+import InputComponent from "./components/InputComponent";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { initStore } from "./store/configureStore";
+import { Provider } from "react-redux";
+import Constants from "expo-constants";
+import ListComponent from './components/ListComponent';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={initStore()}>
+      <SafeAreaProvider>
+      <SafeAreaView style={styles.flex}>
+      <View style={{backgroundColor:'#F8F6F8',padding:20,paddingTop:20+ Constants.statusBarHeight, alignItems:'center', justifyContent:'center'}}>
+        <Text style={{color:'#000', fontWeight:'700'}}>Header</Text>
+        <InputComponent />
+      </View>
+      <ListComponent />
+      </SafeAreaView>
+      </SafeAreaProvider>
+      {/* <View style={styles.container}>
+        <InputComponent />
+      </View> */}
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  flex: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
